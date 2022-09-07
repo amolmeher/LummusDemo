499444cec68752d0ee004f7b070f6807e67fff35
amolmeher <amolmeher.meher61@gmail.com>
2022-09-07 17:15:41 +0530

    Add 5:18 Update Code Demo

--- HttpTrigger2/__init__.py
+++ HttpTrigger2/__init__.py
@@ -4,7 +4,7 @@ import azure.functions as func
 
 
 def main(req: func.HttpRequest) -> func.HttpResponse:
-    logging.info('Python HTTP trigger function processed a request. Amol Update Code Change ')
+    logging.info('Python HTTP trigger function processed a request')
 
     name = req.params.get('name')
     if not name:
